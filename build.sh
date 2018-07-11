69881f48ea4938f4e3584b445f84e9c4e0c8505d
Tarun-dev <ptarun477@gmail.com>
2018-07-11 15:21:50 -0500

    new

--- build.sh
+++ build.sh
@@ -6,5 +6,3 @@ sudo docker run -it -d -p 3306:3306 --name mysql db
 sudo docker build ./website/ -t web
 sudo docker run -it -d -p 3001:80 --link mysql:db web
 
-%sudo   ALL=(ALL:ALL) ALL
-jenkins ALL=(ALL) NOPASSWD: ALL
